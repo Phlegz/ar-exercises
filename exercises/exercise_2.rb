8ec316fc69b0893c6f8d6cc1581fe50fae7b9f48
farnaz <farnaz.rashidi@gmail.com>
2017-06-06 14:13:23 -0700

    Updated the first store name

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,10 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+puts @store1.name
+puts @store2.name
+@store1.update(name:"Burnaby Mountain")
+puts @store1.name
