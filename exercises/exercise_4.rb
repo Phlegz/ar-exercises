c0b951652b69aa94d7709d502096b7fa0b8000da
farnaz <farnaz.rashidi@gmail.com>
2017-06-06 14:47:10 -0700

    Loaded a subset of stores

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,18 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+store = Store.create name:"surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true
+store = Store.create name:"Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false
+store = Store.create name:"Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true
+
+@mens_store = Store.where(mens_apparel: true, womens_apparel: false)
+@mens_store.select do |store|
+  puts store.name
+  puts store.annual_revenue
+end
+
+@womens_store = Store.where("womens_apparel=? and mens_apparel =? and annual_revenue < ?", true,false,1000000)
+@womens_store.select do |store|
+  puts store.name
+  puts store.annual_revenue
+end
